ac9366e6a44835c1c0190253a3a5d5d37d752393
tanzimul <tanzimul.tanim@gmail.com>
2024-06-17 09:29:03 +0200

    Some component added with Dummy data

---
+++ client/src/components/filter/Filter.jsx
@@ -0,0 +1,7 @@
+import "./Filter.scss";
+
+function Filter() {
+	return <div className="filter">I am Filter</div>;
+}
+
+export default Filter;
--- client/src/pages/categoryPage/CategoryPage.jsx
+++ client/src/pages/categoryPage/CategoryPage.jsx
@@ -1,7 +1,24 @@
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
+import { categoriesData } from "../../lib/dummydata";
 import "./CategoryPage.scss";
 
 function CategoryPage() {
-	return <div className="categoryPage">Category Page Updated</div>;
+	const data = categoriesData;
+
+	return (
+		<div className="categoryPage">
+			<div className="mapContainer">Map</div>
+			<div className="listContainer">
+				<div className="listWrapper">
+					<Filter />
+					{data.map((item) => (
+						<Card key={item.id} item={item} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default CategoryPage;
