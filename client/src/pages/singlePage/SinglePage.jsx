4567533905c081e52713fb9aa8e53fb687e2933e
tanzimul <tanzimul.tanim@gmail.com>
2024-06-17 22:06:54 +0200

    Single page added

--- client/src/pages/singlePage/SinglePage.jsx
+++ client/src/pages/singlePage/SinglePage.jsx
@@ -1,7 +1,32 @@
 import "./SinglePage.scss";
+import Map from "../../components/map/Map";
 
 function SinglePage() {
-	return <div className="singlePage">SinglePage</div>;
+	return (
+		<div className="singlePage">
+			<div className="mainData">
+				<div className="profileData">
+					<h3>User Data</h3>
+					<h4>Name: John Doe</h4>
+					<h4>Email : sample@gmail.com</h4>
+					<h4>Phone : +49*********</h4>
+				</div>
+				<div className="details">
+					<h3>Details</h3>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum ea
+						et, magnam incidunt quas saepe reprehenderit nobis quis impedit!
+						Quam impedit quis ab quidem, quo animi eos placeat tempore
+						architecto.
+					</p>
+				</div>
+			</div>
+
+			<div className="mapSingle">
+				<Map />
+			</div>
+		</div>
+	);
 }
 
 export default SinglePage;
